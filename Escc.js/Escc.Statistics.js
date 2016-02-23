a83e774a462ffaab8d81763e0204cfdc4b1ffe38
Rick Mason <richard.mason@eastsussex.gov.uk>
2016-02-23 15:43:42 +0000

    Use jQuery rather than $ to avoid conflicts

--- Escc.js/Escc.Statistics.js
+++ Escc.js/Escc.Statistics.js
@@ -1,30 +1,32 @@
 ﻿if (typeof (jQuery) != 'undefined')
 {
+    // jQuery is used throughout rather than $ to avoid conflicts, particularly in WordPress sites.
+
     // Examples of how to call this method:
-    // $(function () { Escc.Statistics.TrackWithGoogleAnalytics(); });
-    // $(function () { Escc.Statistics.TrackWithGoogleAnalytics({ domains: ['eastsussex.gov.uk', 'eastsussexcc.gov.uk'] }); });
+    // jQuery(function () { Escc.Statistics.TrackWithGoogleAnalytics(); });
+    // jQuery(function () { Escc.Statistics.TrackWithGoogleAnalytics({ domains: ['eastsussex.gov.uk', 'eastsussexcc.gov.uk'] }); });
     var Escc = { Statistics: {
         TrackWithGoogleAnalytics: function (options)
         {
             // Allow option for specific domains to be treated as internal
             var defaultOptions = { domains: [] }
-            options = $.extend(defaultOptions, options);
+            options = jQuery.extend(defaultOptions, options);
             options.domains.push(document.location.host);
 
             var len = options.domains.length;
             for (var i = 0; i < len; i++) options.domains[i] = options.domains[i].toLowerCase();
 
             // Track clicks on links with these extensions using Google Analytics
-            $("a")
-            .filter(function () { return /(\.pdf|\.rtf|\.doc|\.docx|\.dot|\.dotx|\.xls|\.xlsx|\.xlt|\.xltx|\.csv|\.mp3|\.wmv|\.mov|\.xml|\.ashx|\.rss|\.ical|\.hcard|\.ppt|\.pptx|\.pps|\.ppsx|\.pot|\.potx)$/i.test($(this).attr('href')); })
+            jQuery("a")
+            .filter(function () { return /(\.pdf|\.rtf|\.doc|\.docx|\.dot|\.dotx|\.xls|\.xlsx|\.xlt|\.xltx|\.csv|\.mp3|\.wmv|\.mov|\.xml|\.ashx|\.rss|\.ical|\.hcard|\.ppt|\.pptx|\.pps|\.ppsx|\.pot|\.potx)$/i.test(jQuery(this).attr('href')); })
             .click(function ()
             {
                 // Track as virtual page view from Oct 2011 (not as event, because that doesn't support goals or in-page analytics)
-                if (typeof (ga) !== 'undefined') ga('send', 'pageview', $(this).attr("href").toLowerCase());
+                if (typeof (ga) !== 'undefined') ga('send', 'pageview', jQuery(this).attr("href").toLowerCase());
             });
 
             // Act on all external links
-            $("a[href^='http']").filter(function ()
+            jQuery("a[href^='http']").filter(function ()
             {
                 // Selector only targets absolute links. Isolate domain using the slashes that must be there.
                 var href = this.getAttribute('href');
@@ -45,7 +47,7 @@
             .each(function ()
             {
                 // Add rel="external"
-                var link = $(this);
+                var link = jQuery(this);
                 var rel = link.attr("rel");
                 if (typeof (rel) == 'undefined') rel = "";
                 if (!rel || rel.indexOf("external") == -1)
@@ -58,14 +60,14 @@
             .click(function ()
             {
                 // Track in Google Analytics
-                if (typeof(ga) !== 'undefined') ga('send', 'event', 'external links', document.URL, $(this).attr("href"), 0);
+                if (typeof (ga) !== 'undefined') ga('send', 'event', 'external links', document.URL, jQuery(this).attr("href"), 0);
             });
 
             // Track mailto: links in Google Analytics
             // Include them with "external links" so that it's fewer clicks to see where a user went from a page. Can separate the types by filtering on "mailto" if needed.
-            $("a[href^='mailto:']").click(function ()
+            jQuery("a[href^='mailto:']").click(function ()
             {
-                if (typeof(ga) !== 'undefined') ga('send', 'event', 'external links', document.URL, $(this).attr("href"), 0);
+                if (typeof (ga) !== 'undefined') ga('send', 'event', 'external links', document.URL, jQuery(this).attr("href"), 0);
             });
         }
     }
